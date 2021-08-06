126083b84a544d1f9d13164c47f5a64b0c19e176
Bang <332365525@qq.com>
2021-08-06 20:01:17 +0800

    first commit

---
+++ src/main/java/com/atguigu/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author bangchen
+ * @create 2021-08-06 19:57
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
