f57f9d9e37b07f9c397eb0a5d70fd6c598c74b50
Bang <332365525@qq.com>
2021-08-06 20:03:44 +0800

    second commit

--- src/main/java/com/atguigu/GitTest.java
+++ src/main/java/com/atguigu/GitTest.java
@@ -6,6 +6,8 @@ package com.atguigu;
  */
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+
     }
 }
